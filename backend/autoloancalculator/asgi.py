96f5ecc6c25fb36cc3600b4e7094c1188499010b
Roberto Mitsuaki Saito <robertomitsuakisaito@Robertos-MBP.attlocal.net>
2024-05-30 22:51:24 -0400

    Django calculator app created

--- backend/autoloancalculator/asgi.py
+++ backend/autoloancalculator/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for loancalculator project.
+ASGI config for autoloancalculator project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loancalculator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoloancalculator.settings')
 
 application = get_asgi_application()
--- backend/autoloancalculator/wsgi.py
+++ backend/autoloancalculator/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for loancalculator project.
+WSGI config for autoloancalculator project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'loancalculator.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'autoloancalculator.settings')
 
 application = get_wsgi_application()
