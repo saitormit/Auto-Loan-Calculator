49cad8fd9350c045e740c297b9f36bd1cf2f1c25
Roberto Mitsuaki Saito <robertomitsuakisaito@Robertos-MBP.attlocal.net>
2024-06-17 22:01:09 -0400

    Date and refinance parameters handled

--- backend/calculator/forms.py
+++ backend/calculator/forms.py
@@ -6,3 +6,9 @@ class LoanForm(forms.Form):
     loan_duration = forms.IntegerField(label="Loan duration (months)")
     interest = forms.FloatField(label="Interest rate (%)")
     down_payment = forms.IntegerField(label="Down payment ($)")
+    start_date = forms.DateField(input_formats=['%m/%d/%Y'],
+                                 widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}))
+    refi_interest = forms.FloatField(label="New interest rate (%)", required=False)
+    refi_loan_duration = forms.IntegerField(label="New loan duration (months)", required=False)
+    refi_start_date = forms.DateField(input_formats=['%m/%d/%Y'],
+                                 widget=forms.TextInput(attrs={'placeholder': 'MM/DD/YYYY'}), required=False)
