b277df6eab6dac208eade1f4ec5f5d7464c38589
Roberto Mitsuaki Saito <robertomitsuakisaito@Robertos-MBP.attlocal.net>
2024-10-16 20:18:38 -0400

    URL comments added

--- calculator/urls.py
+++ calculator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/", views.say_hello),
-    path("", views.calculate_loan, name="calculate_loan"),
+    path("hello/", views.say_hello), # URL for test page
+    path("", views.calculate_loan, name="calculate_loan"), # URL for the main page
     path('delete/<int:loan_id>/', views.delete_loan, name='delete_loan'),  # URL for deleting a loan calculation
 ]
